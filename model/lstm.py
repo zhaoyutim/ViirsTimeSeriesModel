8a34e8ecd17558899228a1a9a6abe3656a5864dd
赵宇 <zhaoyu@zhaoyudeMBP.lan>
2021-11-01 16:00:50 +0100

    finish GRU model

--- model/lstm.py
+++ model/lstm.py
@@ -8,9 +8,15 @@ if __name__ == '__main__':
     MAX_EPOCHS = 100
     dataset = np.load('../data/proj3_training.npy').transpose((1,0,2))
     print(dataset.shape)
+
+    positive_sample = dataset[(dataset[:,:,45]>0).any(axis=1)]
+    negative_sample = dataset[(dataset[:,:,45]==0).any(axis=1)]
+    negative_sample = negative_sample[np.random.choice(negative_sample.shape[0], positive_sample.shape[0])]
+    dataset = np.concatenate((positive_sample,negative_sample), axis=0)
     y_dataset = np.zeros((dataset.shape[0],dataset.shape[1],2))
     y_dataset[: ,:, 0] = dataset[:, :, 45] > 0
     y_dataset[:, :, 1] = dataset[:, :, 45] == 0
+
     x_train, x_test, y_train, y_test = train_test_split(dataset[:,:,:45], y_dataset, test_size=0.2)
     gru = GRUModel()
     # TODO: Normalization only on train data
